703170117282f3eca2787461a7823d2535c79b37
Paschal Obba <paschalobba@gmail.com>
2024-03-26 17:55:44 +0000

    add tracing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use serde::Deserialize;
+use tower_http::trace::TraceLayer;
 use std::fs;
 
 use axum::{debug_handler, extract::State, routing::get, Json, Router};
 
 use serde_json::{json, Value};
+use tracing_subscriber;
 
 const EMPTY: &'static str = "";
 
@@ -24,12 +26,13 @@ async fn res(State(state): State<String>) -> Json<Value> {
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     let file_path = if cfg!(target_os = "macos") {
         "config.toml"
     }else{
         "/usr/bin/config.toml"
     };
-    
+
     let content = match fs::read_to_string(file_path) {
         Ok(f) => f,
         Err(_) => EMPTY.to_string()
@@ -42,7 +45,8 @@ async fn main() {
 
     let app = Router::new()
         .route("/", get(res))
-        .with_state(data.passwords.wifi);
+        .with_state(data.passwords.wifi)
+        .layer(TraceLayer::new_for_http());
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
